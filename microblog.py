b9efa8b2b58631ad97f1a9f2870972a8984ceaaa
Joshua Smith (CW) <josmith@starbucks.com>
2017-12-31 12:08:17 -0800

    first commit

---
+++ microblog.py
@@ -0,0 +1,3 @@
+#set FLASK_APP=microblog.py (run this in cmd before running the script, it sets a needed environment variable.
+from app import app
+#use "flask run" in cmd to run this, not python microblog.py
