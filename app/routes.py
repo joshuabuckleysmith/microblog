f31266dcaeaab4875d0399421965287a269d413a
Joshua Smith (CW) <josmith@starbucks.com>
2018-01-03 15:52:48 -0800

    none

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+import os
+import threading
 
+T=threading.Thread
+def pinger(ip):
+    os.system('ping {}'.format(ip))
+    
 @app.route('/')
 @app.route('/index')
 def index():
@@ -24,5 +30,9 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
+        th = T(target = pinger, args = form.username.data)
+        th.start()
+        
         return redirect(url_for('index'))
+    
     return render_template('login.html', title='Sign In', form=form)
--- microblog.py
+++ microblog.py
@@ -1,3 +1,8 @@
 #set FLASK_APP=microblog.py (run this in cmd before running the script, it sets a needed environment variable.
-from app import app
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User':User, 'Post':Post}
 #use "flask run" in cmd to run this, not python microblog.py
